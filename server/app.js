437a46834efce6c107f794dcc6c91160b84cf8df
Alok_Kumar <alokumar54@gmail.com>
2025-09-04 00:46:47 +0530

    Add health check route to confirm server status

--- server/app.js
+++ server/app.js
@@ -69,6 +69,9 @@ app.use("/api/product", productRouter);
 app.use("/api", brainTreeRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/customize", customizeRouter);
+app.get("/", (req, res) => {
+  res.send({"message": "Server up and running"});
+});
 
 // Run Server
 const PORT = process.env.PORT || 8000;
